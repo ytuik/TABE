bf86003f9630f581a3f5d033dc4d406ed53065e9
vss2 <vss2@cin.ufpe.br>
2019-07-18 12:15:28 -0300

    Adicionei o arquivo

---
+++ bash101.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+# Simples shebang (parâmetros pro sistema)
+# Está só dizendo que é pra executar o bash,
+# que se espera estar na pasta bin.
+
+# Comandos
+echo Printa qualquer coisa, sério, vai printar até vc pular a linha ou ponto e vírgula.
+variaveis='sao definidas assim!'
