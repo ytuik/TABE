6d1ad0154fd71288957288ec68ea4876c6eae57f
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-09-23 09:18:47 -0300

    Update ess.sh

--- ess.sh
+++ ess.sh
@@ -1,3 +1,11 @@
 mkdir ESS
+cd ESS
 npm install @angular/cli
-PATH=$PATH:/home/CIN/vss2/Documents/node_modules/@angular/cli/bin
+caminho="$pwd/node_modules/@angular/cli/bin"
+PATH=$PATH:$caminho
+
+git clone https://github.com/pauloborba/teachingassistant
+cd teachingassistant/
+
+git checkout -b SaaS1 e670da4c7dda011225d4a6942149cfdcde6e8667
+git reset --hard e670da4c7dda011225d4a6942149cfdcde6e8667
