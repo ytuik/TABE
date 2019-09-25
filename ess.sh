2399932adc7982c80e296f9a29ba674efad49491
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-09-25 10:55:40 -0300

    Update ess.sh

--- ess.sh
+++ ess.sh
@@ -1,3 +1,5 @@
+# Tem que rodar como . ./ess.sh
+
 mkdir ESS
 cd ESS
 npm install @angular/cli
@@ -10,5 +12,19 @@ cd teachingassistant/
 git checkout -b SaaS1 e670da4c7dda011225d4a6942149cfdcde6e8667
 git reset --hard e670da4c7dda011225d4a6942149cfdcde6e8667
 
+# Tenho que ajustar o PATH (olhar parte comentada)
+
 ng new BibPET-gui
 npm start
+
+
+##################################################
+#caminho="${PWD}/node_modules/@angular/cli/bin"
+
+##PATH=$PATH:$caminho
+##echo "Path atualizado:$PATH"
+#tempPATH=$PATH:$caminho
+#echo $tempPATH
+#PATH=""
+##echo $"export PATH=$PATH:$tempPATH" >> ~/.bashrc
+#source ~/.bashrc
