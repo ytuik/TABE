3556918f383d6844bf4e04663aee892be2f693f9
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-11-11 22:42:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ var arrayPontos = [];
 var retaAtual = 0;
 var pontoAtual = 0;
 
+var retaSelect = 0;
+
 var comecoBool = false;
 
 function comeco(){
@@ -21,26 +23,37 @@ function comeco(){
   } else if (estadoAtual == "Nova Curva") {
     // Caso: Pode começar a desenhar, nova reta a ser criada
 
-    // Nova reta, reset de pontos
+    // Nova reta, reset de pontos usados anteriormente
     retaAtual++;
     arrayPontos = []
     
     document.getElementById("nCButton").value = "Parar Curva";
 
   } else {
-    // Caso: Para de desenhar, botão está "Parar Curva", desenha a linha
+    // Caso: Para de desenhar, botão estava "Parar Curva" e foi apertado, temos que desenhar a linha
 
     document.getElementById("nCButton").value = "Nova Curva";
     
     // Trava a canvas para nao desenhar mais enquanto nao aperta "Nova Curva"
+
     comecoBool = false;
+
+    // Guardar o array completo de pontos dessa reta
+    array.push(arrayPontos);
 
     // Lembrando que ao final, tem que redesenhar TUDO, ou seja, FOR de FOR para resenhar pontos e linhas
     //limparTela();
 
-    //for(let a = 0; a < array.length; a++){
-    drawLine(arrayPontos);
-    //}
+    for(let reta = 0; reta < array.length; reta++){
+      if (reta != retaSelect){
+        drawLine(array[reta]);
+      } else {
+        for(let pontos = 0; pontos < array[reta].length; pontos++){
+          drawPointSelect(array[reta][pontos].x, array[reta][pontos].y);
+        }
+        drawLineSelect(arrayPontos);
+      }
+    }
   }
 }
 
@@ -48,11 +61,11 @@ function mux(){
   // 1 = botao esquerdo
   // 3 = botao direito
   if (event.which == 1 && comecoBool == true){
-	let xClick = event.clientX - rect.left;
+	  let xClick = event.clientX - rect.left;
     let yClick = event.clientY - rect.top;
 
     arrayPontos.push({x: xClick, y: yClick});
-    array.push(arrayPontos[pontoAtual++]);
+    //array.push(arrayPontos[pontoAtual++]);
 
     drawPoint(xClick, yClick);
    
@@ -76,6 +89,15 @@ function drawPoint(x, y){
   context.stroke();
 }
 
+function drawPointSelect(x, y){ 
+  context.beginPath();
+  context.arc(x, y, 3, 0, 2 * Math.PI, true);
+  context.moveTo(x, y);
+  context.fill();
+  context.strokeStyle = "#00FF00";
+  context.stroke();
+}
+
 function drawLine(arrayPontos){
   for(r = 1; r < arrayPontos.length; r++){
     let x2 = arrayPontos[r-1].x;
@@ -86,9 +108,31 @@ function drawLine(arrayPontos){
     context.fill();
     context.stroke();
   }
-
 }
 
+function drawLineSelect(arrayPontos){
+  for(r = 1; r < arrayPontos.length; r++){
+    let x2 = arrayPontos[r-1].x;
+    let y2 = arrayPontos[r-1].y;
+    context.moveTo(arrayPontos[r].x, arrayPontos[r].y);
+    context.lineTo(x2, y2);
+    context.strokeStyle = "#0000FF";
+    context.fill();
+    context.stroke();
+  }
+}
+
+function proximaReta(){
+  if(retaSelect < array.length){
+    retaSelect++;
+  }
+}
+
+function anteriorReta(){
+  if(retaSelect > 0){
+    retaSelect--;
+  }
+}
 
 function limparTela(){
   context.fillStyle = "#FFFFFF";
